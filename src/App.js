fc6191ebc782cc0f1af2cd4cdc62264f078cbfd2
ffuertes <felipe.fuertes@gmail.com>
2017-11-16 16:17:44 -0400

    refactor: Books object shape

--- src/App.js
+++ src/App.js
@@ -11,108 +11,114 @@ import './App.css';
 class BooksApp extends Component {
 
   state = {
-    books: {
-      all: [],
-      byIds: {},
-      currentlyReading: [],
-      wantToRead: [],
-      read: []
-    },
+	books: {
+	  all: [],
+	  byIds: {},
+	  currentlyReading: [],
+	  wantToRead: [],
+	  read: []
+	},
   }
 
   componentDidMount() {
-    BooksAPI.getAll().then( ( books ) => {
-        this.setState({
-          books: this.normalize( books )
-        });
-    });
+	BooksAPI.getAll().then( ( books ) => {
+		this.setState({
+		  books: this.process( books )
+		});
+	});
   }
 
-  normalize = ( books ) => {
-    let data = {
-      all: [],
-      byIds: {},
-      currentlyReading: [],
-      wantToRead: [],
-      read: []
-    };
+  /**
+   * @description Process the books returned from the API.
+   * @param {array} books - array of books
+   * @return {object} Books ordered by ids and shelves.
+   */
+  process = ( books ) => {
+	let data = {
+	  all: [],
+	  byIds: {},
+	  currentlyReading: [],
+	  wantToRead: [],
+	  read: []
+	};
 
-    data.all = books;
-    books.map( book => { 
-      data.byIds[book.id] = book;
-      switch ( book.shelf ) {
-        case 'currentlyReading':
-          data.currentlyReading.push(book.id);
-          break;
-        case 'wantToRead':
-          data.wantToRead.push(book.id);
-          break;
-        case 'read':
-          data.read.push(book.id);
-          break;
-        default:
-          break;
-      }
-      return book.id; 
-    });
+	data.all = books;
 
-    return data;
+	books.map( book => {
+	  data.byIds[book.id] = book;
+	  switch ( book.shelf ) {
+		case 'currentlyReading':
+		  data.currentlyReading.push(book.id);
+		  break;
+		case 'wantToRead':
+		  data.wantToRead.push(book.id);
+		  break;
+		case 'read':
+		  data.read.push(book.id);
+		  break;
+		default:
+		  break;
+	  }
+	  return book.id;
+	});
+
+	return data;
   }
 
   updateShelf = ( book, shelf ) => {
-    BooksAPI.update( book, shelf )
-      .then( () => this.setState( ( prevState ) => {
-          let books = prevState.books.all.map( (b) => {
-            b.shelf = b.id === book.id ? shelf : b.shelf;
-            return b;
-          });
-          return { books: this.normalize( books ) }
-        })
-      );
+	BooksAPI.update( book, shelf )
+	  .then( () => this.setState( ( prevState ) => {
+		  let books = prevState.books.all.map( (b) => {
+			b.shelf = b.id === book.id ? shelf : b.shelf;
+			return b;
+		  });
+		  return { books: this.process( books ) }
+		})
+	  );
   }
 
   addBook = ( book, shelf ) => {
-    BooksAPI.update( book, shelf )
-      .then( () => {
-        book.shelf = shelf;
-        this.setState( ( prevState ) => ({
-            books: this.normalize( [ ...prevState.books.all, book ] )
-          })
-        )
-      });
+	BooksAPI.update( book, shelf )
+	  .then( () => {
+		book.shelf = shelf;
+		this.setState( ( prevState ) => ({
+			books: this.process( [ ...prevState.books.all, book ] )
+		  })
+		)
+	  });
   }
 
   render() {
 
-    const labels = {
-      currentlyReading: 'Reading',
-      wantToRead: 'Want to Read',
-      read: 'Read',
-      none: 'None'
-    }
+	const labels = {
+	  currentlyReading: 'Reading',
+	  wantToRead: 'Want to Read',
+	  read: 'Read',
+	  none: 'None'
+	}
 
-    return (
-      <MuiThemeProvider>
-      <div className='app'>
-        <Route exact path='/' render={() => (
-          <ListBooks 
-            labels={labels}
-            books={ this.state.books } 
-            onMoveBook={ this.updateShelf } />
-        )} />
-        <Route path='/add' render={({ history }) => (
-          <Search 
-            labels={labels}
-            booksByIds={ this.state.books.byIds } 
-            onMoveBook={ ( book, shelf ) => {
-              this.addBook( book, shelf );
-              history.push('/');
-            }} />
-        )} />
-      </div>
-      </MuiThemeProvider>
-    )
+	return (
+	  <MuiThemeProvider>
+	  <div className='app'>
+		<Route exact path='/' render={() => (
+		  <ListBooks
+			labels={labels}
+			books={ this.state.books }
+			onMoveBook={ this.updateShelf } />
+		)} />
+		<Route path='/add' render={({ history }) => (
+		  <Search
+			labels={labels}
+			booksByIds={ this.state.books.byIds }
+			onMoveBook={ ( book, shelf ) => {
+			  this.addBook( book, shelf );
+			  history.push('/');
+			}} />
+		)} />
+	  </div>
+	  </MuiThemeProvider>
+	)
   }
 }
 
-export default BooksApp
+export default BooksApp;
